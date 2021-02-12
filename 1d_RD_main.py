cb37c7448b59b1b554acf210f0117cc555d8ec3a
necbal <61414566+necbal@users.noreply.github.com>
2021-02-12 17:50:57 +1300

    -

--- 1d_RD_main.py
+++ 1d_RD_main.py
@@ -6,6 +6,7 @@ v_t = d_v*Lapace v + 1 -  u*u*v
 Neumann BC
 """
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 import random as rnd
 #from matplotlib import colors
@@ -17,21 +18,25 @@ d_v = .02
 uast = 1.1
 vast = pow(1/uast,2)
 
-steps = pow(10,6)
-gs = 400
+
+gs = 200 #changing to 400 does not chenge the plot much. What is the relation to the choice of dh?
 
 
-dh = pow(1/(gs*100),1/2) #at least .5 milion steps
+dh = 5*pow(10,-3) #pow(1/(gs*100),1) #at least .5*10^6  steps
 dt = pow(dh,2)
-"""
+steps = math.floor(pow(10,4)/dt)
+"""  10 000 = steps *dt
 dt = .0001
 dh = dt#1/gs
+
+dh = pow(1/(gs*100),1/2) # gs=400 dh = 5*10^(-4) then #at least .5 milion steps
+dt = pow(dh,2)
 """
 #xi_u = rnd.uniform(0,1)*pow(10,-2)*3
 #xi_v = rnd.uniform(0,1)*pow(10,-2)*3
 """
 dh = 1/gs #1/gs   dt/dh^2 < 1/0.0004 < 1/.04    dt < dh^2/0.0004   dt = dh^2/0.0008
-dt = pow(dh,2)/0.0008#.0001
+dt = pow(dh,2)/0.0008#.0001. calculate the point in time using D dt / dh^2 = .5
 """
 
 Grid_u = [uast+rnd.uniform(0,1)*pow(10,-3) for i in range(0,gs)]  #np.array([uast+xi_u]*(gs*gs)).reshape(gs,gs)
@@ -41,12 +46,12 @@ Next_u = [uast]*gs
 Next_v = [vast]*gs 
 
 
-steps_plot = 1000
+steps_plot = pow(10,5)
 for t in range(0,steps):
         printtime = t/steps_plot
         if printtime.is_integer() == True:
             plt.plot([i/gs for i in range(len(System['u']))],System['u'])
-            plt.title('u: t = ' + str(t))
+            plt.title('u: t = ' + str(round(t *dt,2)))
             plt.show()
         for i in range(1,gs-1):
                 Next_u[i] = System['u'][i] + (pow(dh,-2)*d_u*Laplace1d(System['u'],i)+.1-System['u'][i] +pow(System['u'][i] ,2)*System['v'][i] )*dt
